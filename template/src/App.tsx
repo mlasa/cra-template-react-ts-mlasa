a842fc1900a3b35cf9c50595ab53577dfc2f221a
Marcella A.S.A <marcella.amorimsa@gmail.com>
2022-04-30 19:09:56 -0300

    first commit

---
+++ template/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./styles/globals.scss";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>react-mlasa-template</h1>
+			<small>by Mlasa</small>
+		</div>
+	);
+}
+
+export default App;
